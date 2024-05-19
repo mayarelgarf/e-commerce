93b8b5a0db35a11777cb615012a996a7fd31920e
mayarelgarf <mayarmahmoud123@gmail.com>
2024-05-19 10:46:45 +0300

    highlight low in stock products

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -5,18 +5,26 @@ import { API_URLS } from 'src/assets/http';
 import { IProduct } from '../interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ProductsService {
- private  PRODUCT_APIS  = API_URLS.products
-  constructor(private _http:HttpClient) { }
+  private PRODUCT_APIS = API_URLS.products;
+  constructor(private _http: HttpClient) {}
 
-  getProducts():Observable<IProduct[]>{
-    return this._http.get<IProduct[]>(this.PRODUCT_APIS.getProducts).pipe(take(1))
+  getProducts(): Observable<IProduct[]> {
+    return this._http
+      .get<IProduct[]>(this.PRODUCT_APIS.getProducts)
+      .pipe(take(1));
   }
 
   editProductQuantity(productId: number, quantity: number): Observable<any> {
-    return this._http.post<any>(`${this.PRODUCT_APIS.editProduct}/${productId}`, { AvailablePieces: quantity });
+    return this._http.patch<any>(`${this.PRODUCT_APIS.editProduct}${productId}`, {
+      ProductId: 123,
+      AvailablePieces: quantity,
+      ProductPrice: 123.5,
+      ProductImg:
+        'https://www.decolore.net/wp-content/uploads/2017/04/product-mock-up-set-2.jpg',
+      id: 'ed5d',
+    });
   }
 }
